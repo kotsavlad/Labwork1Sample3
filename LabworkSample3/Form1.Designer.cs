ea680dae6dcf10594d67aa6f222d7205fc316e59
kotsa <kotsa@MYASUSVIVOBOOK>
2022-02-24 17:54:32 +0200

    Add project files.

---
+++ LabworkSample3/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace LabworkSample3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.arrayListBox = new System.Windows.Forms.ListBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.itemTextBox = new System.Windows.Forms.TextBox();
+            this.runButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // arrayListBox
+            // 
+            this.arrayListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.arrayListBox.FormattingEnabled = true;
+            this.arrayListBox.ItemHeight = 20;
+            this.arrayListBox.Items.AddRange(new object[] {
+            "12",
+            "45",
+            "6",
+            "8",
+            "15",
+            "9",
+            "100"});
+            this.arrayListBox.Location = new System.Drawing.Point(44, 60);
+            this.arrayListBox.Margin = new System.Windows.Forms.Padding(4);
+            this.arrayListBox.Name = "arrayListBox";
+            this.arrayListBox.Size = new System.Drawing.Size(306, 364);
+            this.arrayListBox.TabIndex = 0;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.addButton.Location = new System.Drawing.Point(436, 159);
+            this.addButton.Margin = new System.Windows.Forms.Padding(4);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(175, 40);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clearButton.Location = new System.Drawing.Point(436, 259);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(175, 45);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // itemTextBox
+            // 
+            this.itemTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.itemTextBox.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.itemTextBox.Location = new System.Drawing.Point(436, 60);
+            this.itemTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.itemTextBox.Name = "itemTextBox";
+            this.itemTextBox.Size = new System.Drawing.Size(174, 27);
+            this.itemTextBox.TabIndex = 3;
+            this.itemTextBox.Text = "10";
+            this.itemTextBox.WordWrap = false;
+            // 
+            // runButton
+            // 
+            this.runButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.runButton.Location = new System.Drawing.Point(436, 371);
+            this.runButton.Margin = new System.Windows.Forms.Padding(4);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(175, 45);
+            this.runButton.TabIndex = 4;
+            this.runButton.Text = "Run";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Array";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(432, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Item";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.addButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 492);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.runButton);
+            this.Controls.Add(this.itemTextBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.arrayListBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Small numbers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox arrayListBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.TextBox itemTextBox;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LabworkSample3/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabworkSample3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(itemTextBox.Text, out double value))
+            {
+                arrayListBox.Items.Add(value);
+            }
+            else
+            {
+                MessageBox.Show("Invalid item", "Value error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                itemTextBox.SelectAll();
+                itemTextBox.Focus();
+            }
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            arrayListBox.Items.Clear();
+        }
+
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            var array = new double[arrayListBox.Items.Count];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = double.Parse(arrayListBox.Items[i].ToString());
+            }
+            var resultList = GetResult(numbers: array);
+            if (resultList.Count == 0)
+            {
+                MessageBox.Show("No solution", "No numbers found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            var resultStr = string.Join("\n", resultList);
+            MessageBox.Show(resultStr, "Solution", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        /// <summary>
+        /// Search array items which are less than at least 2 previous array elements.
+        /// </summary>
+        /// <param name="numbers">Input numeric array.</param>
+        /// <returns>The list of sought numbers.</returns>
+        private List<double> GetResult(double[] numbers)
+        {
+            var result = new List<double>();
+            for (int i = 2; i < numbers?.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    if (numbers[i] < numbers[j])
+                        count++;
+                    if (count == 2)
+                    {
+                        result.Add(numbers[i]);
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
